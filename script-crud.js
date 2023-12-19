d89def6dc1bd8da0e24c3c16b0b2e4d1111358f3
Elaine Bastos <elaineb80@hotmail.com>
2023-12-19 15:47:13 -0300

    Adicionando tarefas

---
+++ script-crud.js
@@ -0,0 +1,20 @@
+// adicionar tarefa
+
+const btAdicionarTarefa = document.querySelector('.app__button--add-task');
+const formAdicionarTarefa = document.querySelector('.app__form-add-task');
+const textArea = document.querySelector('.app__form-textarea');
+
+const tarefas = [];
+
+btAdicionarTarefa.addEventListener('click', ()=>{
+    formAdicionarTarefa.classList.toggle('hidden');
+})
+
+formAdicionarTarefa.addEventListener('submit', (evento)=>{
+    evento.preventDefault();
+    const tarefa = {
+        descricao: textArea.value
+    }
+    tarefas.push(tarefa)
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+})
